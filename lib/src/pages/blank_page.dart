6a6591835fe7f1cd5e4a38e09d22d7adb893069d
aconcha <kevin.concha.regatto@outlook.com>
2022-03-02 00:14:33 -0500

    Veterinarias Page

--- lib/src/pages/blank_page.dart
+++ lib/src/pages/blank_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:pet_app/src/theme/color_theme.dart';
 import 'package:pet_app/src/widgets/custom_divider.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class BlankPage extends StatelessWidget {
   const BlankPage({Key? key}) : super(key: key);
@@ -23,6 +25,139 @@ class BlankPage extends StatelessWidget {
             ),
           ),
           CustomDivider(),
+          SizedBox(height: 20),
+          ExpansionTile(
+            title: Text("Veterinaria ABC"),
+            subtitle: Text("12:00 -18:00"),
+            leading: Container(
+              decoration: BoxDecoration(
+                color: ColorTheme.primary,
+                borderRadius: BorderRadius.circular(8),
+              ),
+              width: 40,
+              height: 40,
+            ),
+            children: [
+              Container(
+                height: 40,
+                margin: EdgeInsets.symmetric(vertical: 10),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(
+                      Icons.location_on_outlined,
+                      color: ColorTheme.primary,
+                      size: 40,
+                    ),
+                    VerticalDivider(
+                      thickness: 2,
+                    ),
+                    Icon(
+                      Icons.phone,
+                      color: ColorTheme.primary,
+                      size: 40,
+                    ),
+                    VerticalDivider(
+                      thickness: 2,
+                    ),
+                    FaIcon(
+                      FontAwesomeIcons.whatsapp,
+                      color: ColorTheme.primary,
+                      size: 40,
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
+          ExpansionTile(
+            title: Text("Veterinaria ABC"),
+            subtitle: Text("12:00 -18:00"),
+            leading: Container(
+              decoration: BoxDecoration(
+                color: ColorTheme.primary,
+                borderRadius: BorderRadius.circular(8),
+              ),
+              width: 40,
+              height: 40,
+            ),
+            children: [
+              Container(
+                height: 40,
+                margin: EdgeInsets.symmetric(vertical: 10),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(
+                      Icons.location_on_outlined,
+                      color: ColorTheme.primary,
+                      size: 40,
+                    ),
+                    VerticalDivider(
+                      thickness: 2,
+                    ),
+                    Icon(
+                      Icons.phone,
+                      color: ColorTheme.primary,
+                      size: 40,
+                    ),
+                    VerticalDivider(
+                      thickness: 2,
+                    ),
+                    FaIcon(
+                      FontAwesomeIcons.whatsapp,
+                      color: ColorTheme.primary,
+                      size: 40,
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
+          ExpansionTile(
+            title: Text("Veterinaria ABC"),
+            subtitle: Text("12:00 -18:00"),
+            leading: Container(
+              decoration: BoxDecoration(
+                color: ColorTheme.primary,
+                borderRadius: BorderRadius.circular(8),
+              ),
+              width: 40,
+              height: 40,
+            ),
+            children: [
+              Container(
+                height: 40,
+                margin: EdgeInsets.symmetric(vertical: 10),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(
+                      Icons.location_on_outlined,
+                      color: ColorTheme.primary,
+                      size: 40,
+                    ),
+                    VerticalDivider(
+                      thickness: 2,
+                    ),
+                    Icon(
+                      Icons.phone,
+                      color: ColorTheme.primary,
+                      size: 40,
+                    ),
+                    VerticalDivider(
+                      thickness: 2,
+                    ),
+                    FaIcon(
+                      FontAwesomeIcons.whatsapp,
+                      color: ColorTheme.primary,
+                      size: 40,
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
         ],
       ),
     );
